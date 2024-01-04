b2713c04d9bca726fda9ce43d10981c1b0414779
hombo-desu <tmih22336@stu.tamagawa.ac.jp>
2024-01-04 16:05:56 +0900

    種をまくプログラム
    モーションはまだです

--- Assets/Script/Player_Animetion.cs
+++ Assets/Script/Player_Animetion.cs
@@ -16,12 +16,25 @@ public class Player_Animetion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        run += Player_Move.PlayerSpead/1000f;
-        if (run >= (Mathf.PI * 2))
+        if (!Player_Move.finish.Equals("gameOver"))
         {
-            run = run % (Mathf.PI*2);
+            run += Player_Move.PlayerSpead / 1000f;
+            if (run >= (Mathf.PI * 2))
+            {
+                run = run % (Mathf.PI * 2);
+            }
+            float y = Mathf.Cos(run) / 2;
+            float x = this.transform.position.x;
+            if (Player_Move.finish.Equals("clear"))
+            {
+                x += Player_Move.PlayerSpead/1000;
+            }
+            this.transform.position = new Vector3(x, -1.5f - y, 0);
         }
-        float y = Mathf.Cos(run)/2;
-        this.transform.position = new Vector3(this.transform.position.x,-1.5f-y,0);
+        else
+        {
+            this.transform.position = new Vector3(this.transform.position.x+0.05f, this.transform.position.y + 0.05f, 0);
+            this.transform.rotation *= Quaternion.Euler(0f,0f,20f);
+        }
     }
 }
--- Assets/Script/Player_Move.cs
+++ Assets/Script/Player_Move.cs
@@ -6,6 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.ConstrainedExecution;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player_Move : MonoBehaviour
 {
@@ -19,6 +20,8 @@ public class Player_Move : MonoBehaviour
     private static float enemySpead = 0;
     private static float enemyDistance = 300;
     private static float playerPos = 0;
+
+    private static float resultTime = 3f;
 
     //Geter
     public static float PlayerSpead
@@ -39,14 +42,16 @@ public class Player_Move : MonoBehaviour
     }
 
     //定数(バランス調整用)
-    private const float ENEMY_ACCEL = 2.5f;
+    private const float ENEMY_ACCEL = 10f;
     private const float TIME_CONST = 60;
+    private const float ENEMY_FIRST_DISTANCE = 1000;
     private const float PLAYER_ACCEL = 1000;
     private const float PLAYER_DECEL = 30f;
     private const float PLAYER_DECEL_ASSIST = 100;
 
     //プログラム実行用
     private static string lastButton = "default";
+    public static string finish = "false";
 
 
     // Start is called before the first frame update
@@ -54,32 +59,61 @@ public class Player_Move : MonoBehaviour
     {
         playerSpead = 0;
         enemySpead = 0;
-        enemyDistance = 300;
+        enemyDistance = ENEMY_FIRST_DISTANCE;
         playerPos = 0;
-}
+        lastButton = "default";
+        finish = "false";
+        resultTime = 3f;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        enemySpead += ENEMY_ACCEL * Time.deltaTime;
-        enemyDistance += playerSpead * Time.deltaTime - enemySpead * Time.deltaTime;
-        playerPos += playerSpead / TIME_CONST;
-        playerSpead -= PLAYER_DECEL * Time.deltaTime;
-        if (playerSpead < 0.1)
+        if (CountdownTimer.gameStart && finish.Equals("false"))  
         {
-            playerSpead = 0.0f;
-        }
 
-        //走る制御(正しく入力されたらaccelをtrueにする)
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && !lastButton.Equals("Left"))
-        {
-            lastButton = "Left";
-            playerSpead += (1 / (playerSpead + PLAYER_DECEL_ASSIST)) * PLAYER_ACCEL;
+            enemySpead += ENEMY_ACCEL * Time.deltaTime;
+            enemyDistance += playerSpead * Time.deltaTime - enemySpead * Time.deltaTime;
+            playerPos += playerSpead / TIME_CONST;
+            playerSpead -= PLAYER_DECEL * Time.deltaTime;
+            if (playerSpead < 0.1)
+            {
+                playerSpead = 0.0f;
+            }
+
+            //走る制御(正しく入力されたらaccelをtrueにする)
+            if (Input.GetKeyDown(KeyCode.LeftArrow) && !lastButton.Equals("Left"))
+            {
+                lastButton = "Left";
+                playerSpead += (1 / (playerSpead + PLAYER_DECEL_ASSIST)) * PLAYER_ACCEL;
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow) && !lastButton.Equals("Right"))
+            {
+                lastButton = "Right";
+                playerSpead += (1 / (playerSpead + PLAYER_DECEL_ASSIST)) * PLAYER_ACCEL;
+            }
+            if (Input.GetKeyDown(KeyCode.Space)&&lastButton.Equals("default"))
+            {
+                Score.score += 100;
+            }
+            if (enemyDistance <= 0)
+            {
+                finish = "gameOver";
+                Score.score = 0;
+            }
+            if (playerPos > 5000)
+            {
+                finish = "clear";
+            }
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow) && !lastButton.Equals("Right"))
+        else
+        if(!finish.Equals("false"))
         {
-            lastButton = "Right";
-            playerSpead += (1 / (playerSpead + PLAYER_DECEL_ASSIST)) * PLAYER_ACCEL;
+            resultTime -= Time.deltaTime;
+            if (resultTime<0)
+            {
+                SceneManager.LoadScene("Result");
+            }
         }
     }
 }
